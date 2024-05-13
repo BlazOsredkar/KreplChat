189d7af5023e1178421f4a851720b5b67df6e1ea
Blaž Osredkar <91890538+BlazOsredkar@users.noreply.github.com>
2024-05-13 18:23:44 +0200

    dodal semena

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -15,6 +15,7 @@ class Message extends Model
         'sender_id',
         'group_id',
         'reciever_id',
+        
     ];
 
     public function reciever() {
--- database/factories/MessageFactory.php
+++ database/factories/MessageFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Message>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Group>
  */
 class MessageFactory extends Factory
 {
@@ -16,8 +16,29 @@ class MessageFactory extends Factory
      */
     public function definition(): array
     {
+
+        $sender_id = $this->faker->randomElement([0, 1]);
+        if ($sender_id == 0) {
+            $sender_id = $this->faker->randomElement(\App\Models\User::where('id', '!=', 1)->pluck('id')->toArray());
+            $reciever_id = 1;
+        } else {
+            $reciever_id = $this->faker->randomElement(\App\Models\User::pluck('id')->toArray());
+        }
+
+        $group_id = null;
+        if ($this->faker->boolean(50)) {
+            $group_id = $this->faker->randomElement(\App\Models\Group::pluck('id')->toArray());
+            $group = \App\Models\Group::find($group_id);
+            $sender_id = $this->faker->randomElement($group->users->pluck('id')->toArray());
+            $reciever_id = null;
+        }
+
         return [
-            //
+            'sender_id' => $sender_id,
+            'group_id' => $group_id,
+            'message' => $this->faker->realText(200),
+            'receiver_id' => $reciever_id,
+            'created_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
         ];
     }
 }
